7f8ec2906b88aff4f51804b93d90d9d27817a159
suoru <nkfr4hnifkkkahp@yahoo.co.jp>
2020-07-15 20:29:11 +0900

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid for current version and patch releases of Capistrano
 lock '3.14.1'
 
-set :application, "chat-space"
+set :application, 'chat-space'
 set :repo_url, "git@github.com:suoru/chat-space.git"
 
 # バージョンが変わっても共通で参照するディレクトリを指定
